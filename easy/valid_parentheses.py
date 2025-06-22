5e71b148f0de343f00494c5f0aad102f17f929a2
Ranim Rebai <ranimrebai02@gmail.com>
2025-06-22 21:50:27 +0200

    Create valid_parentheses.py

---
+++ easy/valid_parentheses.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        right = ['(','[','{']
+        
+        l = len(s)
+        ok =l%2==0 and s[0] in right
+        while ok and s!="":
+            i=0
+            if s[i+1] in right:
+                i+=1
+            else:
+                if s[i]=='(':
+                    ok= s[i+1]==')'
+                elif s[i]=='{':
+                    ok = s[i+1]=='}'
+                elif s[i]=='[':
+                    ok = s[i+1]==']'
+                else: ok = False
+            if ok ==False: return ok
+            s = s[:i]+s[i+2:]
+        return ok
+
+
+            
